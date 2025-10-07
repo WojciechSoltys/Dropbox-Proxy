c69b53ca4bee6f69c8d9934e0dab8a2be7bfc54a
WojciechSoltys <solwoj@gmail.com>
2025-10-07 23:11:18 +0200

    Create listFilesRaw.ts

---
+++ api/listFilesRaw.ts
@@ -0,0 +1,17 @@
+import { Dropbox } from "dropbox";
+import type { NextApiRequest, NextApiResponse } from "next";
+import process from "node:process";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { path } = req.query;
+    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN });
+    const files = await dbx.filesListFolder({
+      path: path ? decodeURIComponent(path as string) : "",
+    });
+    res.status(200).json({ entries: files.result.entries });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "List failed" });
+  }
+}
